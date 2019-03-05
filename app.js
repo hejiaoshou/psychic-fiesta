6b61aafe9c47198866bd60ad4159db27ce0675e0
hjs <hdhwr@live.com>
2019-03-05 19:08:58 +0800

    init

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const Koa = require('koa');
-const app = new Koa();
+const serve = require('koa-static');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
 
-app.use(async ctx => {
-    ctx.body = 'Hello World!';
-});
+const app = new Koa();
+const router = new Router();
+
+// var MyRouter = require('./route')
+
+// 静态文件
+app.use(serve(__dirname + '/public'));
+// 请求体解析
+app.use(bodyParser());
+// 路由
+app.use(router.routes()).use(router.allowedMethods());
+
+router.use('/test',MyRouter)
 
 app.listen(666);
---
+++ route.js
@@ -0,0 +1,8 @@
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+
+const router = new Router();
+
+
+
+module.exports = router
